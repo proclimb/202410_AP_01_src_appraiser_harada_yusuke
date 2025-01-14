db1c8ac383e5955d16214aba2dbfbd1c33469912
yusuke harada <202410ap01_harada@x-ide.sakura.ne.jp>
2025-01-14 12:27:26 +0900

    ファイルマネージャーに遷移してしまう

--- SRC/libArticle.php
+++ SRC/libArticle.php
@@ -260,7 +260,7 @@ function subArticleEdit()
 		</table>
 
 		<a href="javascript:fnArticleEditCheck();"><img src="./images/<?php print $btnImage ?>" /></a>
-		<a href="javascript:form.act.value='fManager';form.submit();"><img src="./images/btn_return.png" /></a>
+		<a href="javascript:form.act.value='article';form.submit();"><img src="./images/btn_return.png" /></a>
 		&nbsp;&nbsp;<a href="javascript:fnArticleDeleteCheck(<?php print $articleNo ?>);"><img src="./images/btn_del.png" /></a>
 	</form>
 <?php
