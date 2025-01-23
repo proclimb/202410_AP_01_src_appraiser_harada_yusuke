eabc940ecf695862b757c31c0dfd25fdf3b469c5
yusuke harada <202410ap01_harada@x-ide.sakura.ne.jp>
2025-01-23 11:06:01 +0900

    #32602案内日(From)の入力チェックが２つある

--- SRC/js/guide.js
+++ SRC/js/guide.js
@@ -11,7 +11,6 @@ function fnGuideEditCheck() {
 		tmp = getEditObject(i, "guideStart");
 		if (tmp.value == '') { alert(msg + "案内日(開始日付)を入力してください"); return; }
 		if (!fnYMDCheck(msg + "開始日付には正しい日付", tmp)) { return; }
-		if (!fnYMDCheck(msg + "開始日付には正しい日付", getEditObject(i, "guideStartDT"))) { return; }
 		if (!fnYMDCheck(msg + "終了日付には正しい日付", getEditObject(i, "guideEndDT"))) { return; }
 		if (isLength(100, msg + "担当", getEditObject(i, "charge"))) { return; }
 		if (isLength(100, msg + "営業店", getEditObject(i, "name"))) { return; }
